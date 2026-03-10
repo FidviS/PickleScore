8872599b43a471a4a1070ef2fd625be695140481
Fidvi Shereef <hellofidvi@gmail.com>
2026-03-10 17:53:34 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Set the greeting once the page loads
-document.getElementById('main-greeting').innerText = "Hello World!";
-
-// Add a little interaction
-const btn = document.getElementById('colorBtn');
-btn.addEventListener('click', () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-});
